dc8039eb32614e5f604f87a70d67681fca70088a
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-07-12 17:41:55 +0530

    Dynamic File Server 1

---
+++ Node 2024/Node-2  05 July 2024  HTTP Basics and Express/Practice/example6/index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 8086;
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    let data = fs.readdirSync("./", "utf-8");
+    console.log(data);
+
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    
+    res.write("<html><head><title>Directory Listing</title></head><body>");
+    data.forEach((el) => {
+      res.write(`<a href="${el}">${el}</a><br>`);
+    });
+    res.end();
+
+  } else if (req.url != "/favicon.ico") {
+    let data = fs.readFileSync(`.${req.url}`, "utf-8")
+    res.end(data);
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
