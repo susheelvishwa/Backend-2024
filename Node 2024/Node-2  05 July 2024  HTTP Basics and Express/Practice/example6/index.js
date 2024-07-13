dfb2f44e2c5997d45f61eb428c125dd1db876c25
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-07-13 18:00:35 +0530

    Dynamic File Server Part 2

--- Node 2024/Node-2  05 July 2024  HTTP Basics and Express/Practice/example6/index.js
+++ Node 2024/Node-2  05 July 2024  HTTP Basics and Express/Practice/example6/index.js
@@ -1,25 +1,60 @@
 const http = require("http");
 const fs = require("fs");
-const PORT = 8086;
+const PORT = 8087;
 
 const server = http.createServer((req, res) => {
   if (req.url == "/") {
-    let data = fs.readdirSync("./", "utf-8");
-    console.log(data);
+    try {
+      let data = fs.readdirSync("./", "utf-8");
+      console.log(data);
 
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    
-    res.write("<html><head><title>Directory Listing</title></head><body>");
-    data.forEach((el) => {
-      res.write(`<a href="${el}">${el}</a><br>`);
-    });
-    res.end();
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
 
+      data.forEach((el) => {
+        let isDirectory = fs.statSync(`./${el}`).isDirectory();
+        let icon = isDirectory ? "&#128193;" : "&#128196;";
+        res.write(`${icon} <a href="${el}">${el}</a><br>`);
+      });
+      res.end("</body></html>");
+    } catch (err) {
+      console.error(err);
+
+      res.writeHead(500, {
+        "Content-Type": "text/html",
+      });
+      res.end("Internal Server Error");
+    }
   } else if (req.url != "/favicon.ico") {
-    let data = fs.readFileSync(`.${req.url}`, "utf-8")
-    res.end(data);
+    try {
+      let filePath = `.${req.url}`;
+      if (fs.statSync(filePath).isDirectory()) {
+        let data = fs.readdirSync(filePath, "utf-8");
+        res.writeHead(200, {
+          "Content-Type": "text/html",
+        });
+
+        data.forEach((el) => {
+          let isDirectory = fs.statSync(`${filePath}/${el}`).isDirectory();
+          let icon = isDirectory ? "&#128193;" : "&#128196;";
+          res.write(`${icon} <a href="${req.url}/${el}">${el}</a><br>`);
+        });
+        res.end();
+      } else {
+        let data = fs.readFileSync(filePath, "utf-8");
+        res.writeHead(200, {
+          "Content-Type": "text/plain",
+        });
+        res.end(data);
+      }
+    } catch (err) {
+      console.error(err);
+      res.writeHead(404, {
+        "Content-Type": "text/html",
+      });
+      res.end("File Not Found");
+    }
   }
 });
 
