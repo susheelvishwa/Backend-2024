926f8bbc023796bc5e716aafe559c5b14399d904
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-08-27 23:21:47 +0530

    fix in import and export useing type:module

---
+++ Node 2024/Node-1  03 July 2024  Introduction to Node/example0.2/index.js
@@ -0,0 +1,6 @@
+import sum from './sum.js';
+
+let sum2 = sum(4, 5)
+console.log(sum2);
+
+
---
+++ Node 2024/Node-1  03 July 2024  Introduction to Node/example0.2/sum.js
@@ -0,0 +1,13 @@
+function sum(a, b) {
+  return a + b;
+}
+
+let sum1 = sum(2, 3);
+console.log(sum1);
+
+
+export default sum;
+
+
+
+
